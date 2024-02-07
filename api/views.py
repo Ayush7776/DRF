c3aafe105b4abc752763944b504a9b34f772c937
Yash <ayushpathak7776@gmail.com>
2024-02-07 21:44:32 +0530

    ModelSerializer [GET,POST] API

--- api/views.py
+++ api/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from .models import Info
-from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializer import *
+from .models import *
 
-# Create your views here.
-def hello(request,*args,**kwargs):
-    modeldata=Info.objects.all().order_by("?").first()
-    data={}
-    if modeldata:
-        data=model_to_dict(modeldata)
-        # data=model_to_dict(modeldata,fields=['Name','Age','City'])
-    return JsonResponse(data)
+@api_view(['GET'])
+def hello(request):
+    modeldata=Info.objects.all()
+    serializerdata=InfoSerializer(modeldata,many=True)
+    return Response({'data':serializerdata.data})
+
+@api_view(['POST'])
+def hi(request):
+    serializer=InfoSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response({'massage':serializer.errors})
+    serializer.save()
+    return Response({'massage':serializer.data})
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
