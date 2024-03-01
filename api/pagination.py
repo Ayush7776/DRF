a26d05011946967fcbcdf978a4a5fd1e3e56e7c7
Yash <ayushpathak7776@gmail.com>
2024-03-01 10:45:30 +0530

    Limit Offset Pagination

--- api/pagination.py
+++ api/pagination.py
@@ -1,19 +1,24 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
-class MyPagination(PageNumberPagination):
+class MyPagination(LimitOffsetPagination):
     
-    # How Many Records Will Display In In Singal Page 
-    page_size=3
+    # How Many Records Will Display In In Singal Page That Will Deside Limit Attribute
+    default_limit=3
 
-    # If We Have To Change The The page Name -->(http://localhost:8000/api/?page=2)
-    page_query_param='Pageno'             #  -->(http://localhost:8000/api/?Pageno=2)
+    # If We Have To Change The The page Name -->(http://localhost:8000/api/?limit=2)
+    limit_query_param='page'             #  -->(http://localhost:8000/api/?page=2)
 
-    # If We Have To Take Permission To User Fectch Records Acording To Your Need
-    # http://localhost:8000/api/?Pageno=1&records=4
-    page_size_query_param='records'
+    # Fectch Records Acording To Your Need Where We Want To Start From Perticular Record 
+    # http://localhost:8000/api/?page=1&offset=4
+    
+    
+    # offset_query_param Use To Change The if we Have To Change offset String
+    offset_query_param='records'
+    # Before http://localhost:8000/api/?page=1&offset=4
+    # After  http://localhost:8000/api/?page=1&records=4
 
     # We Can Set maximum How Many Records Maximum Fetch User
     max_page_size = 4
-    # http://localhost:8000/api/?Pageno=1&records=4 It Work 
-    # http://localhost:8000/api/?Pageno=1&records=5 It Will Not Work
+    # http://localhost:8000/api/?page=1&records=4 It Work 
+    # http://localhost:8000/api/?page=1&records=5 It Will Not Work
     
--- readme.md
+++ readme.md
@@ -5,9 +5,27 @@
 ### 2] To Import Filters Globaly In Settings.py File
 ```
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
 ```
 ### 3]Simpaly Hit That URL  http://localhost:8000/api/?page=2
 
+# How Many Records Will Display In In Singal Page That Will Deside Limit Attribute
+    default_limit=3
+
+# If We Have To Change The The page Name -->(http://localhost:8000/api/?limit=2)
+    limit_query_param='page'          #  -->(http://localhost:8000/api/?page=2)
+
+# Fectch Records Acording To Your Need Where We Want To Start From Perticular Record 
+    # http://localhost:8000/api/?page=1&offset=4
+    
+    
+# offset_query_param Use To Change The if we Have To Change offset String
+    offset_query_param='records'
+    # Before http://localhost:8000/api/?page=1&offset=4
+    # After  http://localhost:8000/api/?page=1&records=4
+
+# We Can Set maximum How Many Records Maximum Fetch User
+    max_page_size = 4
+    # http://localhost:8000/api/?page=1&records=4 It Work 
+    # http://localhost:8000/api/?page=1&records=5 It Will Not Work
